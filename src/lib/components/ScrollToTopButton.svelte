661db694e4dbd8472cd02fc6ab24ca77a1a22bee
Finnvb <finnvb@gmail.com>
2022-11-15 11:33:10 +0100

    ScrollToTopBtn made

---
+++ src/lib/components/ScrollToTopButton.svelte
@@ -0,0 +1,44 @@
+<script type="ts">
+	export let uid: string;
+</script>
+
+<div>
+	<a href={uid}> <div class="btn"><img src="/Arrow.svg" alt="button" /></div></a>
+</div>
+
+<style>
+	.btn {
+		/* background-color: var(--redLight); */
+        background-color:  #da2a2a;
+		width: 5em;
+		height: 5em;
+		border-radius: 50%;
+		transition: 0.5s ease;
+		padding: 1.25em;
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		cursor: pointer;
+		margin: 1rem;
+	}
+
+	.btn:hover {
+		transform: translateY(-10px) scale(1.1);
+	}
+
+	.btn:hover img {
+		transform: translateY(-10px);
+	}
+
+	.btn img {
+		transition: 0.5s ease;
+		width: 100%;
+		height: 100%;
+	}
+
+	@media (max-width: 40rem) {
+		.btn {
+			position: relative;
+		}
+	}
+</style>
