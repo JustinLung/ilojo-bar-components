cb6f82341cf086942b2f10c88434d9034eb030c6
Finnvb <finnvb@gmail.com>
2022-11-15 12:38:11 +0100

    story plate button made

---
+++ src/lib/components/storyPlateBtn.svelte
@@ -0,0 +1,43 @@
+<script type="ts">
+
+    export let link:string;
+    export let linktext:string;
+    export let height:string;
+	export let source:string;
+	export let color:string;
+</script>
+
+ <div class="container">
+    <img src={source} alt="goldplate2" style="height: {height};" />
+    <div class="centered"><a href="/{link}" style="color: {color};" >{linktext}</a></div>
+</div>
+
+
+<style>
+
+.container {
+		position: relative;
+		text-align: center;
+		color: white;
+	
+	}
+	.centered {
+		position: absolute;
+		top: 0;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -60%);
+	}
+
+    img {
+		height: 5rem;	
+		
+	}
+
+    a {
+		text-decoration: none;
+		color: var(--light);
+		font-size: 1.2rem;
+	}
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
+<script>
+    // import StoryPlateBtn from "$lib/components/storyPlateBtn.svelte";
+</script>
+
 <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<!-- <StoryPlateBtn source={"goldplate.png"} link={'/'} linktext={'Discover'} height={'4rem'} color={("#6c5334")} />
+<StoryPlateBtn source={"goldplate2.png"} link={'/'} linktext={'Discover'} height={'4rem'} color={("#FFFFFF")} />
+<StoryPlateBtn source={"goldplate3.png"} link={'/'} linktext={'Discover'} height={'4rem'} color={("#6c5334")} /> -->
