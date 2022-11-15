ba72738e4db70d03569da7245cdfb3f17d6642df
Finnvb <finnvb@gmail.com>
2022-11-15 11:14:52 +0100

    window component made

---
+++ src/lib/components/Window.svelte
@@ -0,0 +1,123 @@
+<script lang="ts">
+	export let source: string;
+	export let storytitle: string;
+</script>
+
+<section>
+	<div class="image-container">
+		<img class="storyimage" src={source} alt="storyimage" />
+	</div>
+
+	<div class="window">
+		<img class="window" src="window.svg" alt="window" />
+		<img class="windowLeft" src="windowLeft.svg" alt="window" />
+		<img class="windowRight" src="windowRight.svg" alt="window" />
+		<div class="test" />
+	</div>
+
+	<h2>{storytitle}</h2>
+
+	<!-- <Button source={"goldplate.png"} link={data.uid} linktext={'Discover'} height={'4rem'} color={("#6c5334")} /> -->
+</section>
+
+<style>
+	section {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+	}
+
+	.image-container {
+		position: absolute;
+		width: 205px;
+		height: 228px;
+        
+	}
+
+	.storyimage {
+		width: 100%;
+		/* height: 100%; */
+        height: 240px;
+		cursor: pointer;
+		object-fit: cover;
+	}
+
+	.window {
+		position: relative;
+		cursor: pointer;
+	}
+
+	.window:hover .windowLeft {
+		transform: rotateY(-180deg);
+	}
+
+	.window:hover .windowRight {
+		transform: rotateY(180deg);
+	}
+
+	.windowLeft {
+		position: absolute;
+		left: 0;
+		bottom: 0;
+		z-index: 20;
+		margin-left: 11px;
+		margin-bottom: 15px;
+		transition: transform 1.5s ease-in-out;
+		cursor: pointer;
+		transform-style: preserve-3d;
+		cursor: pointer;
+		transform: perspective(200px);
+		transform-origin: left;
+	}
+
+	.windowRight {
+		position: absolute;
+		right: 0;
+		bottom: 0;
+		z-index: 20;
+		margin-right: 12px;
+		margin-bottom: 16px;
+		transition: transform 1.5s;
+		cursor: pointer;
+		transform-style: preserve-3d;
+		cursor: pointer;
+		transform: perspective(200px);
+		transform-origin: right;
+	}
+
+	@media (max-width: 40em) {
+		h2 {
+			font-size: 16px;
+			/* width: 22rem; */
+		}
+
+		.window {
+			width: 12rem;
+		}
+
+		.image-container {
+			position: absolute;
+			width: 179px;
+			height: 196px;
+		}
+
+		.windowRight,
+		.windowLeft {
+			width: 90px;
+		}
+		.windowRight {
+			margin-right: 9px;
+			margin-bottom: 10px;
+		}
+
+		.windowLeft {
+			margin-left: 9px;
+			margin-bottom: 10px;
+		}
+
+        .storyimage{
+            height: 210px;
+        }
+	}
+</style>
