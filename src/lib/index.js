28412495e3692fa349b888f18596f1d15adfd06a
JustinLung <justinlung77@gmail.com>
2022-11-16 12:09:27 +0100

    exported functions in index.js

---
+++ src/lib/index.js
@@ -0,0 +1,12 @@
+// Reexport your entry components here
+import Clock from '$lib/components/Clock.svelte'
+import Footer from '$lib/components/Footer.svelte'
+import Header from '$lib/components/Header.svelte'
+import Link from '$lib/components/Link.svelte'
+import Plate from '$lib/components/Plate.svelte'
+import ScrollToTopButton from '$lib/components/ScrollToTopButton.svelte'
+import StoryPlateBtn from '$lib/components/StoryPlateBtn.svelte'
+import Window from '$lib/components/Window.svelte'
+import WindowSection from '$lib/components/WindowSection.svelte'
+
+export { Clock, Footer, Header, Link, Plate, ScrollToTopButton, StoryPlateBtn, Window, WindowSection }
