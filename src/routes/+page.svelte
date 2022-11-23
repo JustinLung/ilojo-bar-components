3e253a31fbe3dd863a7eff4419475badb6a7dc74
JustinLung <justinlung77@gmail.com>
2022-11-23 13:30:10 +0100

    WIP button component made

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Button from '$lib/components/Button.svelte';
+	export let cardTitle: string = 'Card Title';
+	export let cardDescription: string = 'Card Description';
+	export let cardImage: string;
+</script>
+
+<article>
+	<figure>
+		<img src={cardImage} alt={cardTitle} />
+	</figure>
+	<div>
+		<h2>{cardTitle}</h2>
+		<p>{cardDescription}</p>
+		<div>
+			<Button />
+		</div>
+	</div>
+</article>
